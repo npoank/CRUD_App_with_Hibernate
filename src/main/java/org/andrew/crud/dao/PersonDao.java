ee8a9788ca8ab8d6e3215f551df6a5c3a2b74c39
Kulbicki Andrey <kubl-ml@mail.ru>
2023-01-02 13:33:27 +0300

    solution N + 1 problem

--- src/main/java/org/andrew/crud/dao/PersonDao.java
+++ src/main/java/org/andrew/crud/dao/PersonDao.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class PersonDao {
@@ -23,9 +25,18 @@ public class PersonDao {
     public void testNPlus1(){
         Session session = entityManager.unwrap(Session.class);
 
-        List<Person> people = session.createQuery("select p from Person p", Person.class).getResultList();
+//        List<Person> people = session.createQuery("select p from Person p", Person.class).getResultList();
+//
+//        for (Person person : people){
+//            System.out.println("Person " + person.getName() + " has: " + person.getItems());
+//        }
 
-        for (Person person : people){
+
+//      List<Person> people = session.createQuery("select p from Person p left join fetch p.items").getResultList();
+        Set<Person> people1 = new HashSet<>(session.createQuery("select p from Person p left join fetch p.items").
+                getResultList());
+
+        for (Person person : people1){
             System.out.println("Person " + person.getName() + " has: " + person.getItems());
         }
     }
